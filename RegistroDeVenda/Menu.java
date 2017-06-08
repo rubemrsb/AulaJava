55f12132d1bdcc755335a1acb7bb16a5290d71ac
rubemrsb <rubem.rsb@gmail.com>
2017-06-08 20:29:00 -0300

    Create Menu.java

---
+++ RegistroDeVenda/Menu.java
@@ -0,0 +1,14 @@
+package br.luis;
+
+public class Menu {
+	public void exibeMenu(){
+		System.out.println("--------------------");
+		System.out.println("[1] Cadastrar Produto");
+		System.out.println("[2] Cadastrar Vendedor");
+		System.out.println("[3] Procurar Produto");
+		System.out.println("[4] Procurar Vendedor");
+		System.out.println("[5] Fazer Venda");
+		System.out.println("[0] Exit");
+		System.out.println("--------------------");
+	}
+}
